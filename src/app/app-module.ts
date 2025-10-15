8c57bfc946860e369548e50dec2f61389448cd18
Eren Birsin <erenbirsin@hotmail.com>
2025-10-15 16:04:40 +0300

    Bootstrap kütüphanesi eklendi ve AdminLayout component düzenlendi

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { AdminLayout } from './_layouts/admin-layout/admin-layout';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    AdminLayout
   ],
   imports: [
     BrowserModule,
